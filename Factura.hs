c5aeb901f08a2b3615b61c9061334a317b943254
Nelson Canga <Nelsoncanga17@gmail.com>
2019-05-30 17:42:31 +0100

    Add files via upload
    Modulo Factura v.0.1

--- Factura.hs
+++ Factura.hs
@@ -2,17 +2,17 @@
 module Factura where
 
  type CodFactura = Int
- type Qtd = Float
- type Preco_Prod = Float 
+ type Qtd = [Float]
+ type Preco_Prod = [Float]
  type Pago = Float 
  type Troco = Float 
  type Nome = String
- type CodProd = Int
+ type CodProd = [Int]
  type Numero = Int
  type Data = String
+ type Total = Float 
+ type Vendedor = String 
  
- type Factura = (CodFactura,Nome,Numero,CodProd,Qtd,Preco_Prod,Pago,Troco,Data)
+ type Factura = (CodFactura,Vendedor,Nome,Numero,CodProd,Qtd,Preco_Prod,Pago,Total,Troco,Data)
 
  type Facturas = [Factura]
-
-
